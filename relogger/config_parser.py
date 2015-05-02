8a3f094ecf3925b8f6376c5e6881641605cb922c
Xiaming Chen <chenxm35@gmail.com>
2015-05-03 00:02:49 +0800

    Remove bug of config_parser

--- relogger/config_parser.py
+++ relogger/config_parser.py
@@ -28,8 +28,6 @@ A quick example:
 The section name is user-defined and the options of each sections
 currently support `src.port`, `src.file`, `dst.host`, `dst.file`.
 In a section, at least one src-dst pair should be configured,
-The port and file descriptor for source can not co-exist, while
-the destination takes no constraints.
 For port and host descriptors, multiple values are separated by commas.
 
 The CLI parameters behaves in a similar way with the configuration file,
@@ -47,11 +45,10 @@ class conferr(Exception): pass
 def valid_hostname(hostname):
     validIP = r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
     validHN = r"^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
+
     ipregex = re.compile(validIP)
     hnregex = re.compile(validHN)
-    if not ipregex.match(hostname) and not hnregex.match(hostname):
-        return False
-    return True
+    return ipregex.match(hostname) or hnregex.match(hostname)
 
 class RLConfig(object):
     """ The general configuration for Relogger
@@ -79,7 +76,8 @@ class RLConfig(object):
 
             for s in sections:
                 fields = self._get_section_values(config_parser, s)
-                self.flow_table.append(self._assemble_flowtable(fields))
+                thistable = self._assemble_flowtable(fields)
+                self.flow_table.append(thistable)
 
         else:
             src_host = self._get_hosts_from_names(source) if source else None
@@ -87,29 +85,16 @@ class RLConfig(object):
             dst_host = self._get_hosts_from_names(dest) if dest else None
             dst_file = ['file://' + os.path.abspath(ofile)] if ofile else None
 
-            if None not in (source, ifile):
-                raise conferr('You got both socket and file for the source, only one required.')
-            if dest is None and ofile is None:
-                raise conferr('No destination given, at least one required.')
+            if src_host is None and src_file is None:
+                raise conferr('No source detected, at least one required.')
+            if dst_host is None and dst_file is None:
+                raise conferr('No destination detected, at least one required.')
 
             fields = (src_host, src_file, dst_host, dst_file)
             thistable = self._assemble_flowtable(fields)
             self.flow_table.append(thistable)
 
         self._detect_loop()
-
-    def _get_hosts_from_ports(self, ports):
-        return map(lambda x: 'localhost:%d' % int(x.strip()), ports.split(','))
-
-    def _get_hosts_from_names(self, names):
-        result = list()
-        hosts = map(lambda x: x.strip(), names.split(','))
-        for h in hosts:
-            if valid_hostname(h.split(':')[0]):
-                result.append(h if ':' in h else '%s:%d' % (h, self.PORT))
-            else:
-                raise conferr('Invalid hostname: %s' % h.split(':')[0])
-        return result
 
     def _get_section_values(self, config, section):
         """ extract src and dst values from a section
@@ -118,8 +103,8 @@ class RLConfig(object):
             if config.has_option(section, 'src.port') else None
         src_file = [self._get_abs_filepath(config.get(section, 'src.file'))] \
             if config.has_option(section, 'src.file') else None
-        if None not in (src_host, src_file):
-            raise conferr('Section "%s" gets both socket and file for the source' % section)
+        if src_host is None and src_file is None:
+            raise conferr('Section "%s" gets no sources' % section)
 
         dst_host = self._get_hosts_from_names(config.get(section, 'dst.host')) \
             if config.has_option(section, 'dst.host') else None
@@ -130,19 +115,9 @@ class RLConfig(object):
 
         return (src_host, src_file, dst_host, dst_file)
 
-    def _get_abs_filepath(self, ifile):
-        """ validate src or dst file path with self.config_file
-        """
-        assert ifile is not None
-        ifile = self._strip_file_prefix(ifile)
-        if ifile[0] != '/':
-            ifile = os.path.join(os.path.dirname(self.config_file), ifile)
-        return 'file://' + ifile
-
-    def _strip_file_prefix(self, filename):
-        return filename[7:] if filename.startswith('file://') else filename
-
     def _assemble_flowtable(self, values):
+        """ generate a flowtable from a tuple of descriptors.
+        """
         values = map(lambda x: [] if x is None else x, values)
         src = values[0] + values[1]
         dst = values[2] + values[3]
@@ -159,6 +134,34 @@ class RLConfig(object):
             if source in dests:
                 raise conferr('Loops detected: %s --> %s' % (source, source))
 
+    def _get_hosts_from_ports(self, ports):
+        """ validate hostnames from a list of ports
+        """
+        hosts = map(lambda x: 'localhost:%d' % int(x.strip()), ports.split(','))
+        return list(set(hosts))
+
+    def _get_hosts_from_names(self, names):
+        """ validate hostnames from a list of names
+        """
+        result = set()
+        hosts = map(lambda x: x.strip(), names.split(','))
+        for h in hosts:
+            if valid_hostname(h.split(':')[0]):
+                result.add(h if ':' in h else '%s:%d' % (h, self.PORT))
+            else:
+                raise conferr('Invalid hostname: %s' % h.split(':')[0])
+        return list(result)
+
+    def _get_abs_filepath(self, ifile):
+        """ validate src or dst file path with self.config_file
+        """
+        assert ifile is not None
+        ifile = ifile[7:] if ifile.startswith('file://') else ifile
+        if ifile[0] != '/':
+            basedir = os.path.abspath(os.path.dirname(self.config_file))
+            ifile = os.path.join(basedir, ifile)
+        return 'file://' + ifile
+
     @property
     def flowtable(self):
         """ get a flat flow table globally
@@ -167,9 +170,12 @@ class RLConfig(object):
         for table in self.flow_table:
             for k, v in table.items():
                 if k not in ftable:
-                    ftable[k] = v
+                    ftable[k] = set(v)
                 else:
-                    ftable[k] += v
+                    [ftable[k].add(i) for i in v]
+        # convert set to list
+        for k in ftable:
+            ftable[k] = list(ftable[k])
         return ftable
 
     @property
